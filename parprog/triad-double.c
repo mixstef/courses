fb4a14c363ad474267cc974496c9b57167ed50df
mixstef <mixstef@gmail.com>
2022-03-14 23:41:15 +0200

    Updated material for parprog lab 3-2

---
+++ parprog/triad-double.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+// Sample triad benchmark with arrays of doubles
+// compile with: gcc -Wall -O2 triad-double.c -o triad-double -DN=10000 -DR=10000
+
+void get_walltime(double *wct) {
+  struct timeval tp;
+  gettimeofday(&tp,NULL);
+  *wct = (double)(tp.tv_sec+tp.tv_usec/1000000.0);
+}
+
+int main() {
+double *a,*b,*c,*d;
+int i,j;
+double ts,te,mflops;
+
+  // allocate test arrays
+  a = (double *)malloc(N*sizeof(double));
+  if (a==NULL) exit(1);
+  b = (double *)malloc(N*sizeof(double));
+  if (b==NULL) { free(a); exit(1); }
+  c = (double *)malloc(N*sizeof(double));
+  if (c==NULL) { free(a); free(b); exit(1); }
+  d = (double *)malloc(N*sizeof(double));
+  if (d==NULL) { free(a); free(b); free(c); exit(1); }
+  
+  //initialize all arrays - cache warm-up
+  for (i=0;i<N;i++) {
+    a[i]=2.0*i;
+    b[i]=-i;
+    c[i]=i+5.0;
+    d[i]=-7.0*i;
+  }
+ 
+  // get starting time (double, seconds) 
+  get_walltime(&ts);
+  
+  // do triad artificial work
+  for (j=0;j<R;j++) {
+    for (i=0;i<N;i++) {
+      a[i] = b[i]*c[i]+d[i];
+    }
+  }
+ 
+  // get ending time
+  get_walltime(&te);
+  
+  // check result - avoid loop removal by compiler
+   for (i=0;i<N;i++) {
+    if (a[i]!=b[i]*c[i]+d[i]) {
+      printf("Error!\n");
+      break;
+    }
+  }
+ 
+  
+  // compute mflops/sec (2 floating point operations per R*N passes)
+  mflops = (2.0*R*N)/((te-ts)*1e6);
+  
+  printf("MFLOPS/sec = %f\n",mflops);
+  
+  // free arrays
+  free(a); free(b); free(c); free(d);
+  
+  return 0;
+}
+

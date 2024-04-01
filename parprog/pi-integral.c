2eb9f5123c22ff9c4c0d5ed2c6a3f9ead10b0e3d
Michalis Stefanidakis <mixstef@gmail.com>
2024-04-01 12:42:12 +0300

    Updated parprog lab material

--- parprog/pi-integral.c
+++ parprog/pi-integral.c
@@ -1,15 +1,29 @@
+// Example calculating pi value using integration.
+// Compile with: gcc -O2 -Wall pi-integral.c -o pi-integral -DN=10000000
+
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
-// compile with: gcc -O2 -Wall pi-integral.c -o pi-integral
 
+void get_walltime(double *wct) {
+  struct timeval tp;
+  gettimeofday(&tp,NULL);
+  *wct = (double)(tp.tv_sec+tp.tv_usec/1000000.0);
+}
 
-#define N 1000000	// integration steps
 
 int main() {
 double pi,w,sum,x;
+double ts,te;
+
 
   w = 1.0/N;	// integration step
+
+
+  // get starting time (double, seconds) 
+  get_walltime(&ts);
   
   sum = 0.0;
   for (int i=1;i<=N;i++) {
@@ -18,7 +32,13 @@ double pi,w,sum,x;
   }
  
   pi = w*sum;
+
+  // get ending time
+  get_walltime(&te);
+
   
   printf("Computed pi=%.10f\n",pi);
+  printf("Exec Time (sec) = %f\n",te-ts);
+
   return 0;
 }

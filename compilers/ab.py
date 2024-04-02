d5ade26ec17856278ad0c4ac238986236f1b9bf7
Michalis Stefanidakis <mixstef@gmail.com>
2024-04-02 13:24:09 +0300

    Updated material for compilers lab

--- compilers/ab.py
+++ compilers/ab.py
@@ -1,104 +1,106 @@
 """
-Recursive descent parser, using plex as scanner.
+Recursive descent parser example.
 
-Grammer is:
+Grammar is:
 S -> aB
 B -> b | aBb
 """
 
-import plex
+from compilerlabs import Tokenizer,TokenAction,TokenizerError
+
 
 
 # parsing error, a user-defined exception
 class ParseError(Exception):
-	pass
+    pass
 
 
 # class of recursive descent parser
 class MyParser():
 
-	def __init__(self,scanner):
-			
-		self.scanner = scanner
-		
-		# get initial input token
-		self.next_token,self.next_lexeme = self.scanner.read()
+    def __init__(self,scanner):
+            
+        self.scanner = scanner
+        
+        # get initial input symbol
+        self.next_symbol = next(self.scanner)
 
 
-	def match(self,expected):
-	
-		if self.next_token==expected:
-			# proceed to next token
-			self.next_token,self.next_lexeme = self.scanner.read()
+    def match(self,expected):
+    
+        if self.next_symbol.token == expected:
+            # proceed to next token, if not at end-of-text
+            if self.next_symbol.token is not None:
+                self.next_symbol = next(self.scanner)
 
-		else:
-			raise ParseError('Expected {}, found {} instead'.format(expected,self.next_token))
-			
-			
-	def parse(self):
+        else:
+            raise ParseError(f'Syntax error at line {self.next_symbol.lineno} char {self.next_symbol.charpos}: Expected {expected}, found {self.next_symbol.token} instead')
+            
+            
+    def parse(self):
 
-		# call method for starting symbol of grammar
-		self.S()
-		
-		# keep the following to match end-of-text
-		self.match(None)
+        # call method for starting symbol of grammar
+        self.S()
+        
+        # keep the following to match end-of-text
+        self.match(None)
 
 
-	def S(self):
-				
-		if self.next_token=='A_TOKEN':
-			# S -> a B
-			self.match('A_TOKEN')
-			self.B()
-				
-		else:
-			raise ParseError("In S(), expecting A_TOKEN, found {} instead".format(self.next_token))
-		
-	
-	def B(self):
-			
-		if self.next_token=='B_TOKEN':
-			# B -> b
-			self.match('B_TOKEN')
-			
-		elif self.next_token=='A_TOKEN':
-			# B -> a B b
-			self.match('A_TOKEN')
-			self.B()
-			self.match('B_TOKEN')
-			
-		else:
-			raise ParseError("In B(), expecting A_TOKEN or B_TOKEN, found {} instead".format(self.next_token))
-			
-			
+    def S(self):
+                
+        if self.next_symbol.token=='A_TOKEN':
+            # S -> a B
+            self.match('A_TOKEN')
+            self.B()
+                
+        else:
+            raise ParseError(f'Syntax error at line {self.next_symbol.lineno} char {self.next_symbol.charpos}: In S(), expecting A_TOKEN, found {self.next_symbol.token} instead')
+        
+    
+    def B(self):
+            
+        if self.next_symbol.token=='B_TOKEN':
+            # B -> b
+            self.match('B_TOKEN')
+            
+        elif self.next_symbol.token=='A_TOKEN':
+            # B -> a B b
+            self.match('A_TOKEN')
+            self.B()
+            self.match('B_TOKEN')
+            
+        else:
+            raise ParseError(f'Syntax error at line {self.next_symbol.lineno} char {self.next_symbol.charpos}: In B(), expecting A_TOKEN or B_TOKEN, found {self.next_symbol.token} instead')
+            
+            
 # main part of program
 
 
-# create plex lexicon
-lexicon = plex.Lexicon([
-	(plex.Str("a"),"A_TOKEN"),
-	(plex.Str("b"),"B_TOKEN"),
-	(plex.Rep1(plex.Any(" \t\n")),plex.IGNORE)
-	])
+# create tokenizer and define token patterns
+tokenizer = Tokenizer()
+tokenizer.pattern('a','A_TOKEN')
+tokenizer.pattern('b','B_TOKEN')
+tokenizer.pattern(r'\s+',TokenAction.IGNORE)
+tokenizer.pattern('.',TokenAction.ERROR)
 
-# open input file for parsing
-with open("ab.txt","r") as fp:
-	
-	# create plex scanner for file fp
-	scanner = plex.Scanner(lexicon,fp)
-	
-	# create recursive descent parser
-	parser = MyParser(scanner)
-	
-	try:
-		parser.parse()
-		
-	except ParseError as e:
-		_,lineno,charno = scanner.position()
-		print('Syntax error at line:{} char:{}, {}'.format(lineno,charno+1,e))
-				
-	except plex.errors.PlexError:			
-		_,lineno,charno = scanner.position()
-		print("Scanner Error at line {} char {}".format(lineno,charno+1))
+
+# input text
+text = """
+aa aabbb b
+"""    
+    
+    
+# create scanner for input text
+scanner = tokenizer.scan(text)
+
+# create recursive descent parser
+parser = MyParser(scanner)
+
+try:
+    parser.parse()
+    
+except (TokenizerError,ParseError) as e:
+    print(e)
+            
 
 
